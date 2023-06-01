8951d74b4f7c1dfde671757a327b163ad4600f4e
alex <afriesen731@gmail.com>
2023-05-31 23:48:29 -0600

    error handling

--- scripts/content.js
+++ scripts/content.js
@@ -1,64 +1,67 @@
 
-// Create a file input element
-let fileInput = document.createElement("input");
-fileInput.type = "file";
-fileInput.classList = "input";
-fileInput.id = "fileInput";
-fileInput.name = "fileInput";
-
-body = document.body;
-body.appendChild(fileInput);
-
-console.log(fileInput.type);
-
 // Create a FileReader object to read files
 const reader = new FileReader();
 
-// Event listener for when a file is selected
-fileInput.addEventListener('change', function(e) {
-  let selectedFile = e.target.files[0];
+// Function to create and append the file input element
+function createFileInput() {
+  // Create a file input element
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.classList = "input";
+  fileInput.id = "fileInput";
+  fileInput.name = "fileInput";
 
-  // Event handler for when the file is loaded
-  reader.onload = function(e) {
-    let fileContent = e.target.result.trim();
-
-    let textInput = document.getElementById("prompt-textarea");
-    let button = document.querySelector(".right-2");
-
-    // Enable the button if it is disabled
-    let isDisabled = button.hasAttribute("disabled");
-    if (isDisabled) {
-      button.removeAttribute('disabled');
-    }
-
-    // Set the background color of the button
-    button.setAttribute("style", "background-color: rgb(25, 195, 125);");
-
-    // Append the file content to the text input
-    textInput.value += fileContent;
-
-    // Set focus to the text input
-    textInput.focus();
-
-    // Clear the value of the file input
-    fileInput.value = '';
-
-    console.log(textInput);
-  };
-
-  // Read the selected file as text
-  reader.readAsText(selectedFile);
-});
+  // Append the file input to the document body
+  document.body.appendChild(fileInput);
 
 
 
+  // Event listener for when a file is selected
+  fileInput.addEventListener('change', function(e) {
+    let selectedFile = e.target.files[0];
 
+    // Event handler for when the file is loaded
+    reader.onload = function(e) {
+      let fileContent = e.target.result.trim();
 
+      let textInput = document.getElementById("prompt-textarea");
+      let submitButton = document.querySelector(".right-2");
 
+      // Enable the submit button if it is disabled
+      let isDisabled = submitButton.hasAttribute("disabled");
+      if (isDisabled) {
+        submitButton.removeAttribute('disabled');
+      }
 
+      // Set the background color of the submit button
+      submitButton.setAttribute("style", "background-color: rgb(25, 195, 125);");
 
+      // Append the file content to the text input
+      textInput.value += fileContent;
 
+      // Set focus to the text input
+      textInput.focus();
 
+      // Clear the value of the file input
+      fileInput.value = '';
+
+  
+    };
+
+    // Event handler for file reading errors
+    reader.onerror = function(e) {
+      console.error("Error reading file:", e.target.error);
+      let textInput = document.getElementById("prompt-textarea");
+      textInput.value += e.target.error;
+    };
+
+    // Read the selected file as text
+    reader.readAsText(selectedFile);
+  });
+}
+
+// Call the function to create and append the file input element
+createFileInput();
 
 
 
