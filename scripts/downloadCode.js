78609c91269e9af97b5b935a068e2c023721dc6b
alex <afriesen731@gmail.com>
2023-06-02 22:44:17 -0600

    added comments to downloadCode.js

--- scripts/downloadCode.js
+++ scripts/downloadCode.js
@@ -1,4 +1,5 @@
-const loadWait = 1000;
+const loadWait = 1000; // Wait time in milliseconds
+
 const csv =`Language,Extension
 python,.py
 javascript,.js
@@ -26,145 +27,105 @@ powershell,.ps1
 objective,.m
 haxe,.hx
 elixir,.exs
-erlang,.erl`
-const fileExtentionDict = csvToDict(csv)
+erlang,.erl`; // CSV data for language extensions
 
+const fileExtentionDict = csvToDict(csv); // Convert CSV data to a dictionary
 
-
-
-
+// Event listener for click events
 document.addEventListener("click", function(e) {
-    let clickedElement = e.target;
+  let parent = e.target;
+
+  // Check if the clicked element is inside a <pre> element
+  while (parent) {
+    if (parent.tagName == "PRE") {
+
+      let buttonExists = parent.querySelector(".download-code");
 
 
-    let inCodeElement = false;
-
-    let parent = e.target;
-
-    while (parent) {
-        if (parent.tagName == "PRE") {
-
-
-            let buttonExists = parent.querySelector(".download-code");
-            if (!buttonExists) {
-
-                createButton(parent);
-
-            }
-
-        }
-        parent = parent.parentElement;
+      if (!buttonExists) {
+        createButton(parent); // Create a download button for the code block
+      }
     }
 
+    parent = parent.parentElement;
 
-
-    
-
-
+  }
 });
 
 
+
+// Convert CSV data to a dictionary
 function csvToDict(csv) {
-    const lines = csv.split('\n');
-    // const headers = lines[0].split(',');
-    const dict = {};
-  
-    for (let i = 1; i < lines.length; i++) {
-        let currentLine = lines[i].split(',');
-      
-  
-      
-        dict[currentLine[0]] = currentLine[1];
-      
-    }
-  
-    return dict;
+  const lines = csv.split('\n');
+  const dict = {};
+
+  for (let i = 1; i < lines.length; i++) {
+    let currentLine = lines[i].split(',');
+    dict[currentLine[0]] = currentLine[1];
+  }
+
+  return dict;
 }
 
 
 
+
+// Extract text content from a code element
 function extractText(codeElement) {
-    const jQueryCodeElement = $(codeElement);
-    const code = jQueryCodeElement.text();
-    return code;
+  const jQueryCodeElement = $(codeElement);
+  const code = jQueryCodeElement.text();
+  return code;
 }
 
-
-async function checkLoading() {
-    let loadDots = document.querySelector(".text-2xl");
-
-    if (loadDots) {
-        return true;
-    } 
-    else {
-        return false;
-    }
-}
-
-
+// Get the file extension for a given fileType
 function getDocExtention(fileType) {
-    return fileExtentionDict[fileType] || "txt";
+  return fileExtentionDict[fileType] || "txt";
 }
 
-
+// Download the code as a file
 function downloadFile(button) {
-    
+  const buttonParent = button.parentElement;
 
-    const buttonParent = button.parentElement;
+  const fileType = buttonParent.querySelector("span").innerHTML;
+  const extention = getDocExtention(fileType);
 
-    const fileType = buttonParent.querySelector("span").innerHTML;
-    const extention = getDocExtention(fileType);
-
-    const codeElement = buttonParent.parentElement.querySelector("code");
-
-    
-
-    let code = extractText(codeElement);
-
-    
-
-    
+  const codeElement = buttonParent.parentElement.querySelector("code");
+  let code = extractText(codeElement);
 
 
-    const filename = `${fileType}${extention}`;
+  const filename = `${fileType}${extention}`;
 
-    const blob = new Blob([code], { type: 'text/plain' });
-
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = filename;
-  
-    // Programmatically trigger a click event on the link to initiate the file download
-    link.click();
-  
-    // Clean up the URL object
-    URL.revokeObjectURL(link.href);
+  const blob = new Blob([code], { type: 'text/plain' });
 
 
-    
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+
+
+  link.click();
+  URL.revokeObjectURL(link.href);
 }
 
+
+
+
+// Create a download button for a <pre> element
 function createButton(preElement) {
-
-    let codeHeading = preElement.firstElementChild.firstElementChild;
-    let downLoadButtonExists = codeHeading.querySelector(".download-code");
-    if (!downLoadButtonExists) {
-
-        const downLoadButton = document.createElement("button");
-        
-        downLoadButton.classList = "download-code"
-        downLoadButton.innerHTML = "download"
-        codeHeading.appendChild(downLoadButton);
+  let codeHeading = preElement.firstElementChild.firstElementChild;
+  let downLoadButtonExists = codeHeading.querySelector(".download-code");
 
 
+  if (!downLoadButtonExists) {
 
-        downLoadButton.addEventListener("click", function() {
-            downloadFile(downLoadButton);
-        });
-          
-    }
+    const downLoadButton = document.createElement("button");
+    downLoadButton.classList = "download-code";
+    downLoadButton.innerHTML = "download";
 
+    codeHeading.appendChild(downLoadButton);
+
+    downLoadButton.addEventListener("click", function() {
+      downloadFile(downLoadButton);
+    });
+  }
 }
-
-
-  
